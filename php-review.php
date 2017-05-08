dcafb6b0e9dc5c308dd0f75cad9f76929695b786
gerard <gerardvambrocio@gmail.com>
2017-05-08 19:53:48 +0800

    first commit

---
+++ php-review.php
@@ -0,0 +1,55 @@
+<?php
+
+	// $ages = array(
+	// 	"John" => 35,
+	// 	"Mary" => 27,
+	// 	"Bob" => 55
+	// );
+
+	// // echo $ages['John'];
+//-------------------------------
+	// echo count($ages);
+
+
+		// for ($i=0 ; $i < 5; $i++){
+		// 	echo $i;
+		// }
+//-------------------------------
+
+		// $i= 0;
+		// while ($i <= 10) {
+		// 	echo "Number ". $i .'<br/>' ;
+		// 	$i++ ;
+		// 	}	
+//-------------------------------
+		// $numbers = array(12,45,22,34,65);
+
+		// foreach($numbers as $number) {
+		// 	echo 'This is number '. $number . '<br/>';
+		// }
+//-------------------------------		
+		// $ages = array(
+		// "John" => 35,
+		// "Mary" => 27,
+		// "Bob" => 55
+		// );
+
+		// foreach($ages as $name =>  $age) {
+		// 	echo  $name . ' is ' . $age . '<br/>';
+		// }	
+//-------------------------------
+		// function greet($greeting, $name='john'){
+		// 	echo strtoupper($greeting. ' '. $name);
+		// }
+
+		// greet('hi','gerard');
+//-------------------------------
+		$num1 = 30;
+		$num2 = 20;
+
+		if($num1 == 30 || $num ==40){
+			echo "Correct";
+		}else
+		echo "Wrong";
+?>
+
